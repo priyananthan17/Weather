c5bb85f6b5a8b02f6028e71aacd29d2f7e5b82b8
Priyananthan <164217648+priyananthan17@users.noreply.github.com>
2024-06-05 11:58:06 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDzn8dnL-2_MYYBXHwne1AXQUoYWQCaMi0",
-    authDomain: "weather-a8822.firebaseapp.com",
-    projectId: "weather-a8822",
-    storageBucket: "weather-a8822.appspot.com",
-    messagingSenderId: "527129972467",
-    appId: "1:527129972467:web:53982d2878d16fa9607186",
-    measurementId: "G-H9E47CN575"
+    // apiKey: "AIzaSyDzn8dnL-2_MYYBXHwne1AXQUoYWQCaMi0",
+    // authDomain: "weather-a8822.firebaseapp.com",
+    // projectId: "weather-a8822",
+    // storageBucket: "weather-a8822.appspot.com",
+    // messagingSenderId: "527129972467",
+    // appId: "1:527129972467:web:53982d2878d16fa9607186",
+    // measurementId: "G-H9E47CN575"
   };
 
 const app = initializeApp(firebaseConfig);
